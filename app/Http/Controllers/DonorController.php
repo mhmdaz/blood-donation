4c5a61c41d6de882fafbe2ac3a9e93237b07aac5
Muhammad Azeez <muhammadazeez@zoho.com>
2021-09-25 14:13:13 +0530

    auth middleware added for donor controller

--- app/Http/Controllers/DonorController.php
+++ app/Http/Controllers/DonorController.php
@@ -10,7 +10,7 @@ class DonorController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('store');
     }
 
     public function index()
