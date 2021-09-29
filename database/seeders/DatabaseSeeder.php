a4d1278e654979fae09bc106aad76e64a1ff02a8
Muhammad Azeez <muhammadazeez@zoho.com>
2021-09-29 09:23:55 +0530

    db seeder modified

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        \App\Models\District::truncate();
+        \App\Models\State::truncate();
+        \App\Models\BloodGroup::truncate();
+        \App\Models\User::truncate();
+        \Spatie\Permission\Models\Role::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
         // \App\Models\User::factory(10)->create();
 
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
+            BloodGroup::class,
             StateSeeder::class,
             DistrictSeeder::class,
         ]);
